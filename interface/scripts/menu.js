397e421b42ee272d1ed008324ef9844d16b24cc0
Jullius <emi8511@gmail.com>
2013-06-06 21:54:51 -0400

    diseño

--- interface/scripts/menu.js
+++ interface/scripts/menu.js
@@ -4,4 +4,7 @@ function ocultar_todo()
 	document.getElementById('ver_proyectos_cliente').style.display = 'none';
 	document.getElementById('IngresarUsuarios').style.display = 'none';
 	document.getElementById('EliminarPoC').style.display = 'none';
+	document.getElementById('holacliente').style.display = 'none';
+	document.getElementById('holatraductor').style.display = 'none';				
+	document.getElementById('holaadmin').style.display = 'none';
 }
--- interface/scripts/verificarsesion.js
+++ interface/scripts/verificarsesion.js
@@ -19,6 +19,25 @@ function verificarsesion()
 			{
 				location.href="unwin.html";
 			}
+			
+			if (r == 'cliente')
+			{			
+				document.getElementById('menucliente').style.display = 'block';
+				document.getElementById('holacliente').style.display = 'block';
+			}
+			
+			if (r == 'traductor')
+			{			
+				document.getElementById('menutraductor').style.display = 'block';
+				document.getElementById('holatraductor').style.display = 'block';				
+			}
+			
+			if (r == 'admin')
+			{			
+				document.getElementById('menuadmin').style.display = 'block';
+				document.getElementById('holaadmin').style.display = 'block';
+			}
+			
 		}
 	}
 	xmlhttp.open("GET","../../backend/verificarsesion.php",true);	
@@ -40,11 +59,8 @@ function logout()
 	{		
 		if (xmlhttp.readyState==4 && xmlhttp.status==200)
 		{							
-			var r = xmlhttp.responseText;				
-			if (r == '')
-			{
-				location.href="unwin.html";
-			}
+			var r = xmlhttp.responseText;							
+			location.href="unwin.html";			
 		}
 	}
 	xmlhttp.open("GET","../../backend/logout.php",true);	
