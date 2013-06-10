ce78e98d4b79aac3f2f7dd23c0d14c49a6f7d966
LdyNailih <lilian.moreno@gmail.com>
2013-06-09 22:23:25 -0400

    lilo actulizacion

--- backend/Borrar_Traductor.php
+++ backend/Borrar_Traductor.php
@@ -1,28 +1,31 @@
 <?php
+
 include 'connection.php';
 session_start();
 
-$q=$_POST["q"];
-$datos = explode("<tag>",$q);
 
-$nombre = $datos[0];
-$rut = $datos[1];
+	$consulta="SELECT nombre,rut,mail,admin FROM traductor";
+	$idioma[0] = '';	
+	$result = pg_query($consulta) or die ('Consulta fallida: ' . pg_last_error());
+	while ($line = pg_fetch_array($result, null, PGSQL_ASSOC))
+	{
+	 echo $result;	
+		
+		$idioma[$line["nombre"]] = $line["rut"];				
+	}		
+	pg_free_result($consulta);	
+ 		echo '<tag>'.$result;
 
+//$consulta=pg_exec("SELECT * FROM traductor WHERE rut = '".$rut."'");
+//$filas=pg_numrows($consulta);
+//pg_free_result($consulta);					
+//if ($filas == 0)
+//{
+//	echo "El Traductor no Existe ";
 
-$consulta=pg_exec("SELECT * FROM traductor WHERE rut = '".$rut."'");
-$filas=pg_numrows($consulta);
-pg_free_result($consulta);					
-if ($filas == 0)
-{
-	echo "El Traductor no Existe ";
-//	$_SESSION['mail'] = $mail;	
-//	$_SESSION['nombre'] = $nombre;	
-
-//	mkdir("../../usuarios/".$mail);
-
-}
-else
-{
-	pg_exec("DELETE FROM traductor WHERE rut = '".$rut."'");
-	echo "El Traductor ah sido eliminado.";
-}
+//}
+//else
+//{
+//	pg_exec("DELETE FROM traductor WHERE rut = '".$rut."'");
+//	echo "El Traductor ah sido eliminado.";
+//}
